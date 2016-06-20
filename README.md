c5e559b710a184761fb6c7e388d3e9dbe8d9284f
Alec Caputo <ACaputo@Alec-Caputos-MacBook-Pro.local>
2016-06-20 14:42:08 -0400

    added link to email body

---
+++ README.md
@@ -0,0 +1,19 @@
+# Card Finder
+
+Searches newest posts of /r/buildapcsales for sales on the latest Graphics cards like the GTX 1080 and 1070.
+Requires PRAW https://praw.readthedocs.io/en/stable/
+
+Using python 2.xx
+
+
+     ----Update---
+Is now able to accept any subreddit you input and will browse the "new" section
+
+
+
+	------EMAILL FUNCTIONALITY-----
+
+Follow the terminal prompts and input the appropriate information (expecting the sender to be from a gmail account)
+
+Make sure your gmail account is set to "less secure" www.google.com/settings/security/lesssecureaps
+
---
+++ cardfinder.py
@@ -0,0 +1,62 @@
+import praw
+import time
+import smtplib
+
+r = praw.Reddit(user_agent = "Monitor a subreddit and get email notifications when there is a keyword hit")
+
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+
+looking = []
+inputs = ''
+while True:
+	inputs = raw_input("Enter Keywords you are looking for. To add a new one, press enter. To finish type quit: ")
+	if inputs == "quit":
+		break
+	else:
+		looking.append(inputs)
+
+cache= []
+
+print(looking)
+
+subr = raw_input('Enter the name of the subreddit you would like to monitor: ')
+senderInput = raw_input('Enter the email you would like to send from: ')
+recInput = raw_input('Enter the email you would like to receive notifications on: ')
+passInput = raw_input('Enter the sending email address password: ')
+
+
+def find_card():
+	subreddit = r.get_subreddit(subr)
+	for submission in subreddit.get_new(limit=10):
+		isMatch = any(string in submission.title for string in looking)
+		if submission.id not in cache and isMatch:
+
+			print submission.title
+			cache.append(submission.id)
+			sender= senderInput
+			receivers = recInput
+			msg = MIMEMultipart()
+			msg['From'] = sender
+			msg['To'] = receivers
+			msg['Subject'] ='New Sale!'
+			
+			body = submission.title
+			link = submission.permalink
+			msg.attach(MIMEText(body.encode('utf-8'),'plain','utf-8'))
+			msg.attach(MIMEText(link.encode('utf-8'),'plain','utf-8'))
+
+			server = smtplib.SMTP('smtp.gmail.com', 587)
+			server.starttls()
+			server.login(sender, passInput)
+			text = msg.as_string()
+			server.sendmail(sender, receivers, text)
+
+			server.quit()
+
+
+while True:
+	find_card();
+	time.sleep(10)
+	
